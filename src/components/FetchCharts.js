f8af6f5be11f03300a9576ed30e09522a86def52
jonyward <jonyward@gmail.com>
2022-12-18 18:19:49 +0000

    charts fixed, should deploy from github fine

--- src/components/FetchCharts.js
+++ src/components/FetchCharts.js
@@ -3,9 +3,70 @@ import Chart from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import { Pie } from "react-chartjs-2";
 
-const FetchCharts = ({ data }) => {
-  
-  // Component responsible for rendering bar chart and pie chart which visualise nutritional data queried from an external API
+const BarChart = ({ data }) => {
+
+   const [chartData, setChartData] = useState({
+       labels: ["sugar", "fiber","saturated fat","total fat","protein"],
+       datasets: [
+         {
+           label: "per serving in g", 
+           data: [0,0,0,0,0],
+           backgroundColor: ["#ffbb11","#ec02f1","#50AF95","#03ba6f","#2a71d0"],
+         },
+       ],
+
+   });
+   useEffect(() => {
+     const fetchData = async () => {
+       const url =
+         "https://calorieninjas.p.rapidapi.com/v1/nutrition?query=" + data;
+       console.log(url);
+         const options = {
+         method: "GET",
+         headers: {
+           "X-RapidAPI-Key":
+             "96ea32b66bmsh7955cc5bf47b26bp19a375jsn876d3dd20ee4",
+           "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+         },
+       };
+
+       const response = await fetch(url, options);
+       const incomingData = await response.json();
+       console.log(incomingData);
+       let filteredData = [
+         incomingData.items[0].sugar_g,
+         incomingData.items[0].fiber_g,
+         incomingData.items[0].fat_saturated_g,
+         incomingData.items[0].fat_total_g,
+         incomingData.items[0].protein_g,
+       ];
+       let filteredLabels = [
+         "sugar",
+         "fiber",
+         "saturated fat",
+         "total fat",
+         "protein",
+       ];
+       setChartData({
+         labels: filteredLabels,
+         datasets: [
+           {
+             label: "per serving of " + incomingData.items[0].name + " in g",
+             data: filteredData,
+             backgroundColor: [
+               "#ffbb11",
+               "#ec02f1",
+               "#50AF95",
+               "#03ba6f",
+               "#2a71d0",
+             ],
+           },
+         ],
+       });
+       console.log(chartData);
+     };
+     fetchData();
+   }, [data]);
 
   return (<>
     <Bar
@@ -26,7 +87,7 @@ const FetchCharts = ({ data }) => {
     <Pie
       data={data}
     />
-    </>
+  </>
   );
 };
-export default FetchCharts;
+export default BarChart;
